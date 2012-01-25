0b93daa973aaebcfe44784a858bfd64f66a59b3e
Nicholas J. Kain <nicholas@kain.us>
2012-01-25 05:08:54 -0500

    Use the chroot code from ncmlib.

--- chroot.c
+++ chroot.c
@@ -1,46 +1,124 @@
-/*
- * chroot.c - chroots ncron jobs
+/* chroot.c - chroots ndyndns jobs
+ * Time-stamp: <2010-11-03 05:23:56 njk>
  *
- * (C) 2003-2007 Nicholas J. Kain <njk@aerifal.cx>
+ * (c) 2005-2010 Nicholas J. Kain <njkain at gmail dot com>
+ * All rights reserved.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License version 2.1 as published by the Free Software Foundation.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <pwd.h>
+#include <grp.h>
 
+#include "defines.h"
 #include "log.h"
+#include "strl.h"
 
-int imprison(char *path)
+static char chrootd[MAX_PATH_LENGTH] = "\0";
+static char chroot_modified;
+static char chroot_enable = 1;
+
+void disable_chroot(void)
 {
-    int ret;
+    chroot_enable = 0;
+}
 
-    if (path == NULL)
-        return -1;
+int chroot_enabled(void)
+{
+    return chroot_enable;
+}
 
-    ret = chdir(path);
-    if (ret) {
-        log_line("Failed to chdir(%s).  Not invoking job.", path);
+void update_chroot(const char *path)
+{
+	strlcpy(chrootd, path, sizeof chrootd);
+	chroot_modified = 1;
+}
+
+int chroot_exists(void)
+{
+	return chroot_modified;
+}
+
+char *get_chroot(void)
+{
+	return chrootd;
+}
+
+void wipe_chroot(void)
+{
+	memset(chrootd, '\0', sizeof chrootd);
+}
+
+void imprison(const char *path)
+{
+	int ret;
+
+	if (path == NULL)
+        return;
+
+	ret = chdir(path);
+	if (ret) {
+		log_line("Failed to chdir(%s).  Not invoking job.", path);
+		exit(EXIT_FAILURE);
+	}
+
+    if (chroot_enable) {
+        ret = chroot(path);
+        if (ret) {
+            log_line("Failed to chroot(%s).  Not invoking job.", path);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void drop_root(uid_t uid, gid_t gid)
+{
+    if (uid == 0 || gid == 0) {
+        log_line("FATAL - drop_root: attempt to drop root to root?\n");
         exit(EXIT_FAILURE);
     }
 
-    ret = chroot(path);
-    if (ret) {
-        log_line("Failed to chroot(%s).  Not invoking job.", path);
+    if (getgid() == 0) {
+        if (setregid(gid, gid) == -1) {
+            log_line("FATAL - drop_root: failed to drop real gid == root!\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (getuid() == 0) {
+        if (setreuid(uid, uid) == -1) {
+            log_line("FATAL - drop_root: failed to drop real uid == root!\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    /* be absolutely sure */
+    if (getgid() == 0 || getuid() == 0) {
+        log_line("FATAL - drop_root: tried to drop root, but still have root!\n");
         exit(EXIT_FAILURE);
     }
-    chdir("/");
-    return 0;
 }
 
