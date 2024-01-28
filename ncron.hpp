e2ceaa5cbab0635aa1fec8f1b7f3f1340ee5773a
Nicholas J. Kain <nicholas@kain.us>
2024-01-28 14:13:11 -0500

    Get rid of some unused old #defines.

--- ncron.hpp
+++ ncron.hpp
@@ -1,10 +1,7 @@
-// Copyright 2003 Nicholas J. Kain <njkain at gmail dot com>
+// Copyright 2003-2024 Nicholas J. Kain <njkain at gmail dot com>
 // SPDX-License-Identifier: MIT
 #ifndef NCRON_NCRON_HPP_
 #define NCRON_NCRON_HPP_
-
-#define MAXLINE 1024
-#define MAX_BUF 1024
 
 void clock_or_die(struct timespec *ts);
 
